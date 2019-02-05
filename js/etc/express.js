4d99dc84dae111010df59f511cf80beedc1898bd
Terry <38963056+zoegodterry@users.noreply.github.com>
2019-02-04 20:45:42 -0500

    fixed paypal

--- js/etc/express.js
+++ js/etc/express.js
@@ -1,9 +1,11 @@
 const botconfig = require("./../../config.json");
 const stripe = require("stripe")(botconfig.payment_processor.stripe.secret_key);
+const queryString = require('query-string');
 const database = require("./database.js");
 const bodyParser = require("body-parser");
 const sendMail = require("./mailer.js");
 const express = require("express");
+const request = require('request');
 const bot = require("./disc.js");
 const app = express();
 
@@ -43,22 +45,64 @@ app.post("/webhook/endpoint/stripe", function(req, res) {
 });
 
 app.post("/webhook/endpoint/paypal", async function(req, res) {
-    let _event = req.body;
-    if (_event.event_type === "BILLING.SUBSCRIPTION.CREATED") {
-        let key = await database.generate_key({
-            paymentEmail: _event.resource.payer.payer_info.email,
-            customerId: _event.resource.payer.payer_info.payer_id,
-            subscriptionId: _event.resource.id,
-            paymentTimestamp: Date.now(),
-            discordId: ""
-        });
-        // Lets e-mail the customer his/her key.
-        sendMail(_event.resource.payer.payer_info.email, key);
-    } else if (_event.event_type === "BILLING.SUBSCRIPTION.CANCELLED") {
-        // Remove key off database. Will be swept up by on_ready.
-        database.cancel_key(_event.resource.id);
+
+    // Return 200 to caller
+    res.status(200);
+
+    // Read the IPN message sent from PayPal and prepend 'cmd=_notify-validate'
+    let body = 'cmd=_notify-validate&' + req.body;
+
+    let options = {
+        url: 'https://www.sandbox.paypal.com/cgi-bin/webscr',
+        method: 'POST',
+        headers: {
+            'Connection': 'close'
+        },
+        body: body,
+        strictSSL: true,
+        rejectUnauthorized: false,
+        requestCert: true,
+        agent: false
     };
-    res.status(200).json({status: "acknowledged"});
+
+    // POST IPN data back to PayPal to validate
+    request(options, async function callback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+            //Inspect IPN validation result and act accordingly
+            if (body.substring(0, 8) === 'VERIFIED') {
+                // The IPN is verified
+                console.log('Valid IPN.');
+                let parsed = queryString.parse(decodeURIComponent(req.body));
+                switch (parsed.txn_type) {
+                    case "subscr_signup":
+                        let key = await database.generate_key({
+                            paymentEmail: parsed.payer_email,
+                            customerId: parsed.payer_id,
+                            subscriptionId: parsed.subscr_id,
+                            paymentTimestamp: Date.now(),
+                            discordId: ""
+                        });
+                        // Lets e-mail the customer his/her key.
+                        sendMail(parsed.payer_email, key);
+                        break;
+                    case "subscr_eot":
+                        // Remove key off database. Will be swept up by on_ready.
+                        database.cancel_key(parsed.subscr_id);
+                        break;
+                };
+            } else if (body.substring(0, 7) === 'INVALID') {
+                // The IPN invalid
+                console.log('Invalid IPN.');
+            } else {
+                // Unexpected response body
+                console.log(`Unexpected IPN verification body response: ${body}`);
+            };
+        } else {
+            // Unexpected response
+            console.log(`Unexpected IPN verification response: ${response}`);
+        };
+    });
 });
 
 module.exports = server;
